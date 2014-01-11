3d445084bbe41897867fd5d1721b775d7fce29ff
ht4n <hsetiawan@bluekai.com>
2014-01-10 22:52:30 -0800

    + update script to reset to take solr url as parameter

--- solrreset.sh
+++ solrreset.sh
@@ -1,3 +1,6 @@
-curl http://localhost:8983/solr/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml;charset=utf-8'
+#!/bin/bash
 
-curl http://localhost:8983/solr/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
+echo $1
+curl $1 --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml;charset=utf-8'
+
+curl $1 --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
